a7b9b899b02e87b0560392478810a2108a926c35
balgrofen <gellencsabakalman1@gmail.com>
2025-03-11 19:53:51 +0100

    2 Progressbars added and currently working with the phone being portrait mode.
    Compacting the code will be needed.

--- app/src/main/java/com/example/bubblelevel/MainActivity.java
+++ app/src/main/java/com/example/bubblelevel/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.bubblelevel;
 
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    SensorManager sensorManager;
+    Sensor accelerometer;
+
+    SensorEventListener accelerometerEventListener;
+    TextView levelConfirm;
+    ProgressBar tiltProgressBarR;
+    ProgressBar tiltProgressBarL;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(accelerometerEventListener,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(accelerometerEventListener);
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +50,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        levelConfirm=findViewById(R.id.levelConfirm);
+        tiltProgressBarR=findViewById(R.id.levelProgressR);
+        tiltProgressBarL=findViewById(R.id.levelProgressL);
+
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        accelerometer=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        accelerometerEventListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float tiltX = event.values[0]; // Left-Right Tilt
+                int progressR = (int) (0 - (tiltX * 10));
+                progressR = Math.max(0, Math.min(progressR, 100));
+
+                int progressL = (int) (0 + (tiltX * 10));
+                progressL = Math.max(0, Math.min(progressL, 100));
+
+                tiltProgressBarR.setProgress(progressR);
+                tiltProgressBarL.setProgress(progressL);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
     }
 }
